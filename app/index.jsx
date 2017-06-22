81aecb138877217792b2c9af0a1ffad919c30988
Adam Adamczyk <aadamczyk@figaro.com.pl>
2017-06-22 14:43:00 +0200

    add redux counter sample

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './components/Counter'
+import counter from './reducers'
 
-ReactDOM.render(
-    <div>Hello World. React, redux and goLang... and more....</div>,
-    document.getElementById('app')
-);
+const store = createStore(counter)
+const rootEl = document.getElementById('app')
+
+const render = () => ReactDOM.render(
+    <Counter
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    rootEl
+)
+
+render()
+store.subscribe(render)
